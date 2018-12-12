7b356843bf20740e6e99e6958fbcd9fa9ebb0527
charlessxd <charlesr.baldwinn@gmail.com>
2018-12-12 09:56:06 -0600

    README updated; cleanup

--- README.md
+++ README.md
@@ -3,3 +3,42 @@
 [![GoDoc](https://godoc.org/github.com/charlessxd/gosu-wrapper/gosu?status.svg)](https://godoc.org/github.com/charlessxd/gosu-wrapper/gosu) 
 [![GoReport](https://goreportcard.com/badge/github.com/charlessxd/gosu-wrapper)](https://goreportcard.com/report/github.com/charlessxd/gosu-wrapper)
 
+This wrapper was initially intended for personal use, but feel free to use it if you feel it's of use. 
+The osu-api of which this is based on can be found [here](https://github.com/ppy/osu-api/wiki).
+
+
+## Installation
+
+If you don't already have a working Go environment, go to [this page](https://golang.org/doc/install) before continuing.
+
+```sh
+go get github.com/charlessxd/gosu-wrapper/gosu
+```
+
+
+## Usage
+
+Import the package into your project.
+
+```go
+import "github.com/charlessxd/gosu-wrapper/gosu"
+```
+
+## Examples
+Examples can be found in the [examples directory](https://github.com/charlessxd/gosu-wrapper/tree/master/examples).
+##### BeatmapExample.go 
+```go
+s := gosu.NewSession(os.Getenv("API_KEY"))
+
+call := gosu.BeatmapCall{
+	BeatmapID: "252002",
+}
+
+beatmap, _ := s.FetchBeatmap(call)
+
+fmt.Println(beatmap)
+```
+
+## Documentation
+All exported variables and functions have been documented:
+ -  [![GoDoc](https://godoc.org/github.com/charlessxd/gosu-wrapper/gosu?status.svg)](https://godoc.org/github.com/charlessxd/gosu-wrapper/gosu) 
--- gosu/endpoints.go
+++ gosu/endpoints.go
@@ -1,51 +1,6 @@
 package gosu
 
 // Endpoint Constants
-/*const (
-	EndpointOsu    = "https://osu.ppy.sh/"
-	EndpointAPI    = EndpointOsu + "api/"
-	EndpointAPIKey = "k"
-
-	EndpointBeatmaps             = "get_beatmaps?"
-	EndpointBeatmapsSince        = "since"
-	EndpointBeatmapsBeatmapSetID = "s"
-	EndpointBeatmapsBeatmapID    = "b"
-	EndpointBeatmapsUserID       = "u"
-	EndpointBeatmapsMode         = "m"
-	EndpointBeatmapsType         = "type"
-	EndpointBeatmapsConverted    = "a"
-	EndpointBeatmapsHash         = "h"
-	EndpointBeatmapsLimit        = "limit"
-
-	EndpointUser       = EndpointAPI + "get_user?"
-	EndpointUserUserID = "u"
-	EndpointUserMode   = "m"
-	EndpointUserType   = "type"
-
-	EndpointScores          = EndpointAPI + "get_scores?"
-	EndpointScoresBeatmapID = "b"
-	EndpointScoresUserID    = "u"
-	EndpointScoresMode      = "m"
-	EndpointScoresMods      = "mods"
-	EndpointScoresType      = "type"
-	EndpointScoresLimit     = "limit"
-
-	EndpointUserBest       = EndpointAPI + "get_user_best?"
-	EndpointUserBestUserID = "u"
-	EndpointUserBestMode   = "m"
-	EndpointUserBestType   = "type"
-	EndpointUserBestLimit  = "limit"
-
-	EndpointUserRecent       = EndpointAPI + "get_user_recent?"
-	EndpointUserRecentUserID = "u"
-	EndpointUserRecentMode   = "m"
-	EndpointUserRecentType   = "type"
-	EndpointUserRecentLimit  = "limit"
-
-	EndpointMatch   = "get_match?"
-	EndpointMatchID = "mp"
-)*/
-
 const (
 	EndpointOsu               = "https://osu.ppy.sh/"
 	EndpointAPI               = EndpointOsu + "api/"
